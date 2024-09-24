5570b0ae4d2283dbf57983a7b278ae1500afbb4c
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-09-24 20:09:51 +0900

    기본 server 구축

---
+++ server/server.js
@@ -0,0 +1,39 @@
+//server.js
+const express = require('express');
+const app = express();
+const service = require('./service.js');
+const cors = require('cors');
+app.use(cors());
+require('dotenv').config();
+const test = require('./Router/router');
+
+app.use('/api', test);
+
+const port = 3002; //node 서버가 사용할 포트 번호
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
+
+app.get('/temp', async (req, res) => {
+    try {
+        const getTemp = await service.getTemp();
+
+        console.log("res: " + JSON.stringify({ getTemp }));
+        res.json({ getTemp });
+    } catch (error) {
+        console.error("에러 발생: " + error);
+        res.status(500).json({ error: "서버 내부 오류 발생" });
+    }
+});
+
+app.post('api/item/register', async (req, res) => {
+    try {
+        const getTemp = await service.postRegister(req.body.title);
+
+        res.json({response : "ok"});
+    } catch (error) {
+        console.error("에러 발생: " + error);
+        res.status(500).json({ error: "서버 내부 오류 발생" });
+    }
+});
---
+++ server/service.js
@@ -0,0 +1,50 @@
+// service.js
+const mysql = require("mysql");
+require('dotenv').config();
+
+const connect = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+connect.connect((err) => {
+    if (err) {
+        console.error('MySQL 연결 오류:', err);
+        throw err;
+    }
+    console.log('MySQL 연결 성공');
+});
+
+function getTemp() {
+    return new Promise((resolve, reject) => {
+        connect.query('select adddate(curdate(),1) as today',
+            function (error, result) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result[0]);
+                }
+            });
+    });
+}
+
+function postRegister(title, content, imgUrl, keyword) {
+    return new Promise((resolve, reject) => {
+        connect.query('INSERT INTO Posts (title, content) values (?, ?)',
+            [title, content],
+            function (error, result) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+    });
+}
+
+module.exports = {
+    getTemp,
+    postRegister
+};
