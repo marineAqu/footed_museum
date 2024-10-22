91f97865c4463b799d4a9a2d17b67bf172113183
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-10-22 20:23:48 +0900

    /chat, /member API 분리

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const userRouter = require('./router');
+const userRouter = require('./register.router');
+const chatRouter = require('./chat.router');
+const memberRouter = require('./member.router');
 
 router.use('/', userRouter);
+router.use('/', chatRouter);
+router.use('/', memberRouter);
 
 module.exports = router;
