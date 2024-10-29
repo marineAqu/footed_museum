52048ba17c336ede1f4c269e623b52ef5cc00fcf
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-10-29 16:11:30 +0900

    [fix] resister 메소드 수정

--- server/register.controller.js
+++ server/register.controller.js
@@ -1,4 +1,5 @@
 const service = require('./service.js');
+const registerService = require('./register.service');
 const qrCode = require("qrcode");
 require('dotenv').config();
 let multer = require("multer");
@@ -11,13 +12,14 @@ const upload = multer({
 class RegisterController {
     resister = async (req, res) => {
         //TODO: 프론트에서 keyword_id로 변경해서 전달해주거나 이 사이에서 keyword를 keyword_id로 변경하는 과정 추가 필요
+        const { userid, title, keyword, content, status } = await req.body;
+        const getTemp = await registerService.postRegister(userid, title, content, status);
 
-        const getTemp = await service.postRegister(req.body.title, req.body.content, 1, 1);
+        //for (let i = 0; i < req.body.keywordId.length; i++)
+        //    await registerService.postKeyword(req.body.postId, req.body.keywordId[i]);
 
-        for (let i = 0; i < req.body.keywordId.length; i++)
-            await service.postKeyword(req.body.postId, req.body.keywordId[i]);
-
-        res.status(200).json({ getTemp });
+        res.status(200).json({ message: "success" });
+        //res.status(200).json({ getTemp });
     };
 
     reeealTemp = async (req, res) => {
