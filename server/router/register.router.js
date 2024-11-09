6ccb4fa40bb9b3fa0498236fb09584f54da6c35a
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-11-09 20:32:19 +0900

    [fix] multer 수정

--- server/router/register.router.js
+++ server/router/register.router.js
@@ -8,7 +8,7 @@ const multer = require('multer');
 const upload = multer();
 
 registerRouter.get("/tempmakeQr", registerController.makeQrCode);
-registerRouter.post("/tempRegister", upload.none(), registerController.resister);
+registerRouter.post("/tempRegister", upload.single('image'), registerController.resister);
 registerRouter.get("/tempTestDir", registerController.tempTestDir);
 registerRouter.post("/tempReal", registerController.reeealTemp);
 registerRouter.post("/visionTest", upload.single('image'), registerController.visionAPI);
