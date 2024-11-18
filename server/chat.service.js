b31e0bc255a6199f68da67b23255fd0057eb7af2
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-11-18 11:22:29 +0900

    [fix] 조인 쿼리 수정 - 가장 최근 메시지, 게시글 제목 가져오기

--- server/chat.service.js
+++ server/chat.service.js
@@ -9,9 +9,52 @@ const connect = mysql.createConnection({
 });
 
 function chatlist(userid) {
+    //Posts 테이블과 조인
+
     return new Promise((resolve, reject) => {
-        connect.query('select * from ChatRooms where user1_id = ? or user_id2 = ?',
-            [userid, userid],
+        const modifiedQuery = `SELECT 
+    chatrooms.room_id AS roomid,
+    chatrooms.post_id AS postid,  
+    DATE_FORMAT(chatrooms.room_created_at, '%Y년 %m월 %d일 %H시 %i분') AS created_at,
+    chatrooms.user1_id,
+    chatrooms.user_id2,
+    posts.title AS title,
+    posts.content AS content,
+    min(messages.message_content) AS lastMessage
+FROM 
+    chatrooms
+JOIN 
+    posts
+ON 
+    posts.post_id = chatrooms.post_id
+JOIN
+    messages
+ON
+    messages.room_id = chatrooms.room_id
+WHERE 
+    (chatrooms.user1_id = ? OR chatrooms.user_id2 = ?)
+GROUP BY messages.room_id
+    `;
+
+        const query = `select chatrooms.room_id as roomid, chatrooms.post_id as postid, 
+            DATE_FORMAT(room_created_at, \'%Y년 %m월 %d일 %H시 %i분\') AS created_at, user1_id, user_id2, 
+            posts.title as title, posts.content as content
+            from ChatRooms, posts, messages
+            where posts.post_id = chatrooms.post_id
+              and chatrooms.room_id = messages.room_id
+              and (user1_id = ? or user_id2 = ?)
+              and (
+                SELECT
+                    messages.room_id,
+                    MIN(sent_at) AS sent_at
+                FROM
+                    messages
+                GROUP BY
+                    messages.room_id
+            )`;
+
+
+            connect.query(modifiedQuery, [userid, userid],
             function (error, result) {
                 if (error) {
                     reject(error);
